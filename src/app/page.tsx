70dbd9bd440779121fc2698e3bcca8f0264c5476
dscheigert <daniel.scheigert@gmail.com>
2025-06-24 20:30:41 -0700

    style: slight padding adjustment to home for mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import HeroSection from '@/components/ui/HeroSection';
 
 export default function Home() {
     return (
-        <div className="container mx-auto px-4 lg:py-36 py-16">
+        <div className="container mx-auto px-4 lg:py-36 py-8">
             <HeroSection />
         </div>
     );
