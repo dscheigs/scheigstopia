a84fcec99ba116bbc2cf83a96a3ec755a5379542
dscheigert <daniel.scheigert@gmail.com>
2025-06-24 19:54:46 -0700

    feat: expand hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,8 @@ import HeroSection from '@/components/ui/HeroSection';
 
 export default function Home() {
     return (
-        <div className="container mx-auto px-4 py-16">
-            <HeroSection
-                topText="Hi,"
-                bottomText="I'm Daniel"
-                imageSrc="/headshot.jpg"
-                imageAlt="me, daniel"
-            />
+        <div className="container mx-auto px-4 lg:py-36 py-16">
+            <HeroSection />
         </div>
     );
 }
