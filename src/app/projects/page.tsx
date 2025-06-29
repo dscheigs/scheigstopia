4e24b260f9fe1c33ade2003b0506699982859df5
dscheigert <daniel.scheigert@gmail.com>
2025-06-29 09:54:30 -0700

    feat: add projects description

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -14,7 +14,12 @@ export default function Projects() {
 
     return (
         <div className="container mx-auto px-4 py-8">
-            <h1 className="text-page-title mb-16">Projects</h1>
+            <h1 className="text-page-title mb-6">Projects</h1>
+            <p className="text-body text-text-minimal text-center mb-10 max-w-2xl mx-auto">
+                I believe one of the most enjoyable aspects of life is learning
+                new things. Here are some of my various projects that showcase
+                my journey and the skills I&apos;ve acquired along the way.
+            </p>
             <div className="flex justify-center mb-8">
                 <SegmentedControl
                     options={['Professional', 'Personal']}
